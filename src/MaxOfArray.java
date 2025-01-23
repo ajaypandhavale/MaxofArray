c001c936326fd67f27de03b796a359f881b8b498
ajaypandhavale <aj40135821@wipro.com>
2025-01-23 17:59:29 +0530

    Max from the array elements

---
+++ src/MaxOfArray.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class MaxOfArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int limit;
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("enter the number of elements in a array");
+		limit=sc.nextInt();
+		
+		int[] arr = new int[limit];
+		
+		
+		System.out.println("enter the array elements");
+		for(int i=0;i<limit;i++) {
+			 arr[i] = sc.nextInt();
+			 }
+		
+		System.out.println("array elements");
+		int max=0;
+		
+		for(int i=0;i<limit;i++) {
+			if (arr[i] > max) 
+                max = arr[i]; 
+        }
+		
+		
+		System.out.println("max ="+max);
+		
+		
+
+	}
+
+}
